06b9ead23deaa04cb234b0902a5358cee3c1786a
Indra Lukmana <indra@indralukmana.com>
2025-05-16 20:11:10 +0700

    ✨ feat: update build scripts and add output directory configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./app"),
-    },
-  },
+	plugins: [react()].filter(Boolean),
+	build: {
+		outDir: './dist',
+		emptyOutDir: true,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './app'),
+		},
+	},
 }));
